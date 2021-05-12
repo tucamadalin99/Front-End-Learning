152c9804353777fdfc4882c72611f44f63d8acd5
Tuca Madalin <50795013+tucamadalin99@users.noreply.github.com>
2021-05-12 15:54:26 +0300

    ML-1 - Firebase + navigation

---
+++ scripts/auth.js
@@ -0,0 +1,32 @@
+window.onload = () => {
+    var firebaseConfig = {
+        apiKey: "AIzaSyDAURzkKmtpLsBGNYYt1Sj6-pTmk7lh6Wg",
+        authDomain: "ibm-frontend.firebaseapp.com",
+        databaseURL: "https://ibm-frontend-default-rtdb.firebaseio.com",
+        projectId: "ibm-frontend",
+        storageBucket: "ibm-frontend.appspot.com",
+        messagingSenderId: "185203005676",
+        appId: "1:185203005676:web:b77b0a61affd56af9c6dc9",
+        measurementId: "G-0LV3RBN6J8"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    firebase.analytics();
+    console.log("Init");
+    const user = {
+        id: localStorage.getItem("userId"),
+        username: localStorage.getItem("username"),
+        password: localStorage.getItem("password")
+    }
+    let userCart = {};
+    if (user.password === "user" && user.username === "user" && user.id !== null) {
+        console.log(user);
+        const dbRef = firebase.database().ref(`${user.id}`);
+        dbRef.on('value', async (snapshot) => {
+            userCart = await snapshot.val();
+            document.getElementById("cart-link").childNodes[1].nodeValue = ` Cart ($${userCart.cartPrice.toFixed(2)})`
+        })
+    } else {
+        console.log("Auth failed, no user in local storage")
+    }
+}
