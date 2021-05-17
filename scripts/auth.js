50c5e2ee48792371590a0e7e79244e3da0798b20
Tuca Madalin <50795013+tucamadalin99@users.noreply.github.com>
2021-05-17 16:48:22 +0300

    ML-2 - Finished Banner

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,14 +1,14 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDAURzkKmtpLsBGNYYt1Sj6-pTmk7lh6Wg",
+    authDomain: "ibm-frontend.firebaseapp.com",
+    databaseURL: "https://ibm-frontend-default-rtdb.firebaseio.com",
+    projectId: "ibm-frontend",
+    storageBucket: "ibm-frontend.appspot.com",
+    messagingSenderId: "185203005676",
+    appId: "1:185203005676:web:b77b0a61affd56af9c6dc9",
+    measurementId: "G-0LV3RBN6J8"
+};
 window.onload = () => {
-    var firebaseConfig = {
-        apiKey: "AIzaSyDAURzkKmtpLsBGNYYt1Sj6-pTmk7lh6Wg",
-        authDomain: "ibm-frontend.firebaseapp.com",
-        databaseURL: "https://ibm-frontend-default-rtdb.firebaseio.com",
-        projectId: "ibm-frontend",
-        storageBucket: "ibm-frontend.appspot.com",
-        messagingSenderId: "185203005676",
-        appId: "1:185203005676:web:b77b0a61affd56af9c6dc9",
-        measurementId: "G-0LV3RBN6J8"
-    };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
     firebase.analytics();
@@ -29,4 +29,20 @@ window.onload = () => {
     } else {
         console.log("Auth failed, no user in local storage")
     }
+
+    let apiDataRef = firebase.database().ref('api');
+    let data = [];
+    let bannerPic = document.getElementById('banner-pic');
+    let tags = document.querySelectorAll('.tags');
+    let title = document.querySelector('.title');
+    let description = document.querySelector('.description');
+    apiDataRef.on('value', async (snapshot) => {
+        data = await snapshot.val();
+        bannerPic.src = data.results[0].imageUrl;
+        tags.forEach((tag, index) => {
+            tag.childNodes[0].nodeValue = data.results[0].tags[index];
+        })
+        title.textContent = data.results[0].name;
+        description.textContent = data.results[0].description;
+    })
 }
