0f3ac1ab89c76e610b2a7ccbf2f2476621f21129
Tuca Madalin <50795013+tucamadalin99@users.noreply.github.com>
2021-05-20 16:19:57 +0300

    ML-4 - Mobile 2 columns added

--- scripts/auth.js
+++ scripts/auth.js
@@ -160,23 +160,24 @@ window.onload = () => {
             }
             $('.carousel-wrapper').slick({
                 dots: true,
+                mobileFirst: true,
                 arrows: true,
                 infinite: false,
                 speed: 300,
-                slidesToShow: 4,
+                slidesToShow: 2,
                 slidesToScroll: 1,
                 responsive: [
                     {
                         breakpoint: 1024,
                         settings: {
-                            slidesToShow: 3,
-                            slidesToScroll: 3,
+                            slidesToShow: 4,
+                            slidesToScroll: 1,
                             infinite: true,
                             dots: true
                         }
                     },
                     {
-                        breakpoint: 600,
+                        breakpoint: 667,
                         settings: {
                             slidesToShow: 2,
                             slidesToScroll: 2
@@ -186,6 +187,13 @@ window.onload = () => {
                         breakpoint: 480,
                         settings: {
                             slidesToShow: 1,
+                            slidesToScroll: 1
+                        }
+                    },
+                    {
+                        breakpoint: 375,
+                        settings: {
+                            slidesToShow: 2,
                             slidesToScroll: 1
                         }
                     }
